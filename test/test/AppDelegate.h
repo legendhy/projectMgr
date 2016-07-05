1f6d540594a0c91cb35a1af647039bf4d0a1a75e
huangyu <huangyu@huangyudemacbook-pro.local>
2016-07-05 11:18:49 +0800

    test version 1

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by huangyu on 16/7/5.
+//  Copyright © 2016年 huangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by huangyu on 16/7/5.
+//  Copyright © 2016年 huangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
